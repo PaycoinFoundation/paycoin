11ad09167d83b3a20b4176c073d57905a3dc2ad7
Michael Hendricks <michael@ndrix.org>
2012-03-02 12:31:16 -0700

    Reopen debug.log on SIGHUP
    The best log rotation method formerly available was to configure
    logrotate with the copytruncate option.  As described in the logrotate
    documentation, "there is a very small time slice between copying the
    file and truncating it, so some logging data might be lost".
    
    By sending SIGHUP to the server process, one can now reopen the debug
    log file without losing any data.

--- src/init.cpp
+++ src/init.cpp
@@ -101,6 +101,10 @@ void HandleSIGTERM(int)
     fRequestShutdown = true;
 }
 
+void HandleSIGHUP(int)
+{
+    fReopenDebugLog = true;
+}
 
 
 
@@ -186,7 +190,13 @@ bool AppInit2(int argc, char* argv[])
     sa.sa_flags = 0;
     sigaction(SIGTERM, &sa, NULL);
     sigaction(SIGINT, &sa, NULL);
-    sigaction(SIGHUP, &sa, NULL);
+
+    // Reopen debug.log on SIGHUP
+    struct sigaction sa_hup;
+    sa_hup.sa_handler = HandleSIGHUP;
+    sigemptyset(&sa_hup.sa_mask);
+    sa_hup.sa_flags = 0;
+    sigaction(SIGHUP, &sa_hup, NULL);
 #endif
 
     //
