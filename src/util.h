507e90b44144bc0654f4f31103f1c1fddf5f99b3
Philip Kaufmann <phil.kaufmann@t-online.de>
2013-07-24 09:30:09 +0200

    exclude CreatePidFile() function on WIN32 as it is unused

--- src/util.h
+++ src/util.h
@@ -165,7 +165,9 @@ boost::filesystem::path GetDefaultDataDir();
 const boost::filesystem::path &GetDataDir(bool fNetSpecific = true);
 boost::filesystem::path GetConfigFile();
 boost::filesystem::path GetPidFile();
+#ifndef WIN32
 void CreatePidFile(const boost::filesystem::path &path, pid_t pid);
+#endif
 void ReadConfigFile(std::map<std::string, std::string>& mapSettingsRet, std::map<std::string, std::vector<std::string> >& mapMultiSettingsRet);
 bool GetStartOnSystemStartup();
 bool SetStartOnSystemStartup(bool fAutoStart);
