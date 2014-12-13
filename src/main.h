069e77b8cba091e143d99c638eae1a785f3e77b2
Paycoin Dev <paycoin@butterkiss.com>
2014-12-13 06:54:09 -0500

    modified:   src/main.h

--- src/main.h
+++ src/main.h
@@ -51,7 +51,7 @@ static const int STAKE_START_TIME = 1418470264; // Sat 13 Dec 2014 06:31:04 AM E
 //static const int STAKE_START_TIME = 1418345400; // Thu 11 Dec 2014 05:30:00 PM EST
 static const unsigned int POW_START_TIME = 1418403600; // Fri 12 Dec 2014 12:00:00 PM EST
 //static const unsigned int POW_START_TIME = 1418345100; // Thu 11 Dec 2014 05:00:00 PM EST
-static const unsigned int POW_END_TIME = 1419012000; // Fri 19 Dec 2014 01:00:00 PM EST
+static const unsigned int POW_END_TIME = 1419094800; // Sat 20 Dec 2014 12:00:00 PM EST
 //static const unsigned int POW_END_TIME = 1418345700; // Thu 11 Dec 2014 05:40:00 PM EST
 // MODIFIER_INTERVAL: time to elapse before new modifier is computed
 static const unsigned int MODIFIER_INTERVAL = 6 * 60 * 60;
