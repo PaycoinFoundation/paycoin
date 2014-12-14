1b3d64a394d2ba22ab4044707ddfdaef5f126e26
Izerian <izerian@yahoo.com>
2014-12-14 15:43:39 -0500

    Mac net.cpp fix
    Compiler conditional to use boost for the threads array on a mac.

--- src/net.cpp
+++ src/net.cpp
@@ -53,7 +53,11 @@ static bool vfReachable[NET_MAX] = {};
 static bool vfLimited[NET_MAX] = {};
 static CNode* pnodeLocalHost = NULL;
 uint64 nLocalHostNonce = 0;
+#ifdef MAC_OSX
+boost::array<int, THREAD_MAX> vnThreadsRunning;
+#else
 array<int, THREAD_MAX> vnThreadsRunning;
+#endif
 static std::vector<SOCKET> vhListenSocket;
 CAddrMan addrman;
 
