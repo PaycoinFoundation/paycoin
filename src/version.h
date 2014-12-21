e6417e405a5548ac1933889876bbb31a4f257a52
Poramin Insom <paycoin@butterkiss.com>
2014-12-21 14:22:14 -0800

    modified:   src/checkpoints.cpp 	modified:   src/kernel.cpp 	modified:   src/main.cpp 	modified:   src/main.h 	modified:   src/version.h

--- src/version.h
+++ src/version.h
@@ -60,12 +60,12 @@ extern const std::string CLIENT_DATE;
 // network protocol versioning
 //
 
-static const int PROTOCOL_VERSION = 70000;
+static const int PROTOCOL_VERSION = 60004;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
 // NOTE: as of bitcoin v0.6 message serialization (vSend, vRecv) still
 // uses MIN_PROTO_VERSION(209), where message format uses PROTOCOL_VERSION
-static const int MIN_PROTO_VERSION = 70000;
+static const int MIN_PROTO_VERSION = 209;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
