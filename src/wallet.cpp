7b82f6143a553dbb3d521125e3588531beb21781
paycoindev <paycoin@butterkiss.com>
2015-01-01 19:22:48 -0800

    Update wallet.cpp

--- src/wallet.cpp
+++ src/wallet.cpp
@@ -1419,7 +1419,7 @@ bool CWallet::CreateCoinStake(const CKeyStore& keystore, unsigned int nBits, int
                 nCredit += pcoin.first->vout[pcoin.second].nValue;
                 vwtxPrev.push_back(pcoin.first);
                 txNew.vout.push_back(CTxOut(0, scriptPubKeyOut));
-                if (block.GetBlockTime() + nStakeSplitAge > txNew.nTime)
+                if ((block.GetBlockTime() + nStakeSplitAge > txNew.nTime) && (nCredit < MINIMUM_FOR_PRIMENODE))
                     txNew.vout.push_back(CTxOut(0, scriptPubKeyOut)); //split stake
                 if (fDebug && GetBoolArg("-printcoinstake"))
                     printf("CreateCoinStake : added kernel type=%d\n", whichType);
