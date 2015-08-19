5c2a03ae5ac51aee71972816ae2223a54429e8ee
Mitchell Cash <mitchell.cash@gmail.com>
2015-08-19 17:18:09 +1000

    Add code to open (display) debug.log on Windows with the associated application and add a button to the Information page in the Debug console

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -190,11 +190,18 @@ RPCConsole::RPCConsole(QWidget *parent) :
 {
     ui->setupUi(this);
 
+#ifndef WIN32
+    // Show Debug logfile label and Open button only for Windows
+    ui->labelDebugLogfile->setVisible(false);
+    ui->openDebugLogfileButton->setVisible(false);
+#endif
+
     // Install event filter for up and down arrow
     ui->lineEdit->installEventFilter(this);
     ui->messagesWidget->installEventFilter(this);
 
     connect(ui->clearButton, SIGNAL(clicked()), this, SLOT(clear()));
+    connect(ui->openDebugLogfileButton, SIGNAL(clicked()), this, SLOT(on_openDebugLogfileButton_clicked()));
 
     startExecutor();
 
@@ -407,6 +414,11 @@ void RPCConsole::on_tabWidget_currentChanged(int index)
     {
         ui->lineEdit->setFocus();
     }
+}
+
+void RPCConsole::on_openDebugLogfileButton_clicked()
+{
+    GUIUtil::openDebugLogfile();
 }
 
 void RPCConsole::scrollToEnd()
