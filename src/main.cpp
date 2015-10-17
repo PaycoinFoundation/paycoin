eb1b31aec3334e9f72f8cd173f51e04ffc8413b1
Mitchell Cash <mitchell.cash@gmail.com>
2015-10-17 21:54:20 +1000

    Bugfix: remove conflicting transactions from memory pool
    When a transaction A is in the memory pool, while a transaction B (which shares an input with A) gets accepted into a block, A was kept forever in the memory pool.
    
    This commit adds a CTxMemPool::removeConflicts method, which removes transactions that conflict with a given transaction, and all their children.
    
    This results in less transactions in the memory pool, and faster construction of new blocks.
    
    Reference:
    https://github.com/bitcoin/bitcoin/commit/231b399952fd620ee0f72b1947024dba9651630d

--- src/main.cpp
+++ src/main.cpp
@@ -721,7 +721,7 @@ bool CTxMemPool::addUnchecked(const uint256& hash, CTransaction &tx)
 }
 
 
-bool CTxMemPool::remove(CTransaction &tx)
+bool CTxMemPool::remove(const CTransaction &tx, bool fRecursive)
 {
     // Remove transaction from memory pool
     {
@@ -729,6 +729,13 @@ bool CTxMemPool::remove(CTransaction &tx)
         uint256 hash = tx.GetHash();
         if (mapTx.count(hash))
         {
+            if (fRecursive) {
+                for (unsigned int i = 0; i < tx.vout.size(); i++) {
+                    std::map<COutPoint, CInPoint>::iterator it = mapNextTx.find(COutPoint(hash, i));
+                    if (it != mapNextTx.end())
+                        remove(*it->second.ptx, true);
+                }
+            }
             BOOST_FOREACH(const CTxIn& txin, tx.vin)
                 mapNextTx.erase(txin.prevout);
             mapTx.erase(hash);
@@ -738,6 +745,20 @@ bool CTxMemPool::remove(CTransaction &tx)
     return true;
 }
 
+bool CTxMemPool::removeConflicts(const CTransaction &tx)
+{
+    // Remove transactions which depend on inputs of tx, recursively
+    LOCK(cs);
+    BOOST_FOREACH(const CTxIn &txin, tx.vin) {
+        std::map<COutPoint, CInPoint>::iterator it = mapNextTx.find(txin.prevout);
+        if (it != mapNextTx.end()) {
+            const CTransaction &txConflict = *it->second.ptx;
+            if (txConflict != tx)
+                remove(txConflict, true);
+        }
+    }
+    return true;
+}
 
 void CTxMemPool::queryHashes(std::vector<uint256>& vtxid)
 {
@@ -1711,8 +1732,10 @@ bool Reorganize(CTxDB& txdb, CBlockIndex* pindexNew)
         tx.AcceptToMemoryPool(txdb, false);
 
     // Delete redundant memory transactions that are in the connected branch
-    BOOST_FOREACH(CTransaction& tx, vDelete)
+    BOOST_FOREACH(CTransaction& tx, vDelete) {
         mempool.remove(tx);
+        mempool.removeConflicts(tx);
+    }
 
     printf("REORGANIZE: done\n");
 
