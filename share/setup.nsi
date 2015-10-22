4d6f12a322c6c45ceb5302673d49f2a6dfb3cde9
Nathan Bass <sblood86@gmail.com>
2015-10-22 10:54:48 -0500

    Update version and release notes for 0.3.2.0

--- share/setup.nsi
+++ share/setup.nsi
@@ -5,7 +5,7 @@ SetCompressor /SOLID lzma
 
 # General Symbol Definitions
 !define REGKEY "SOFTWARE\$(^Name)"
-!define VERSION 0.3.1.1
+!define VERSION 0.3.2.0
 !define COMPANY "Paycoin project"
 !define URL https://github.com/PaycoinFoundation/paycoin/
 
--- src/version.h
+++ src/version.h
@@ -17,8 +17,8 @@
 // paycoin version - intended for display purpose only
 #define PEERUNITY_VERSION_MAJOR       0
 #define PEERUNITY_VERSION_MINOR       3
-#define PEERUNITY_VERSION_REVISION    1
-#define PEERUNITY_VERSION_BUILD       1
+#define PEERUNITY_VERSION_REVISION    2
+#define PEERUNITY_VERSION_BUILD       0
 
 static const int PEERUNITY_VERSION =
                            1000000 * PEERUNITY_VERSION_MAJOR
