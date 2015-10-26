3556b7f909c43cec2507f3a51cb0e6c292955621
Nathan Bass <sblood86@gmail.com>
2015-10-26 16:33:24 -0500

    Revert "Remove checkpoint-warning:"
    This reverts commit 88fbd4b835597398d40141eaca22a1af0a76fc1b.

--- src/checkpoints.h
+++ src/checkpoints.h
@@ -46,6 +46,7 @@ namespace Checkpoints
     bool SetCheckpointPrivKey(std::string strPrivKey);
     bool SendSyncCheckpoint(uint256 hashCheckpoint);
     bool IsMatureSyncCheckpoint();
+    bool IsSyncCheckpointTooOld(unsigned int nSeconds);
 }
 
 // paycoin: synchronized checkpoint
